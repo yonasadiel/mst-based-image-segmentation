8444c234f015860a3f9896bed0aeecd50ce86e0c
yonasadiel <yonazadielwiguna@gmail.com>
2017-12-04 00:22:03 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# MST Based Image Segmentation
+
+A simple image segmentation, applied to make a [cutout](https://helpx.adobe.com/photoshop/using/filter-effects-reference.html) filter.
+
+## Result
+
+Original | Result
+---------|---------
+![](./img/house.jpg) | ![](./img/house_cutout.jpg)
+![](./img/mountains.jpg) | ![](./img/mountains_cutout.jpg)
+![](./img/balloon.jpg) | ![](./img/balloon_cutout.jpg)
+![](./img/flower.jpg) | ![](./img/flower_cutout.jpg)
+
+## Photo source
+
+* [house](http://www.palmatin.com/wp-content/uploads/2013/06/painted-square-log-house1.jpg)
+* [mountains](https://static.pexels.com/photos/15382/pexels-photo.jpg)
+* [balloon](https://i.ytimg.com/vi/7_Y3QFdmpHw/maxresdefault.jpg)
+* [flower](https://static.pexels.com/photos/36753/flower-purple-lical-blosso.jpg)
---
+++ cutout.py
@@ -0,0 +1,152 @@
+import sys
+import time
+from PIL import Image
+from random import randint
+
+img_row = 0
+img_col = 0
+time_start = 0
+parent = []
+
+def valid(a, b):
+	global img_row
+	global img_col
+	return a < img_row and a >= 0 and b < img_col and b >= 0
+
+def jarak(a,b):
+	c = 0
+	for i in range(3):
+			c += (a[i] - b[i])*(a[i] - b[i])
+	return c
+
+def look_parent(i,j):
+	global parent
+	if (parent[i][j] != (i,j)):
+			parent[i][j] = look_parent(parent[i][j][0], parent[i][j][1])
+	return parent[i][j]
+
+def search(lst, target):
+  min = 0
+  max = len(lst)-1
+  avg = (min+max)//2
+  while (min < max):
+    if (lst[avg] == target):
+      return avg
+    elif (lst[avg] < target):
+      return avg + 1 + search(lst[avg+1:], target)
+    else:
+      return search(lst[:avg], target)
+  return avg
+
+def main(filein, fileout, radius=3, treshold=150):
+	global img_row
+	global img_col
+	global parent
+	global time_start
+
+	# opening image
+	img = Image.open(filein)
+	img_row = img.size[1]
+	img_col = img.size[0]
+
+	in_pixels = list(img.getdata())
+	nodes = [[(0,0,0) for x in range(img_col)] for x in range(img_row)]
+	edges = []
+
+	# creating nodes
+	for i in range(len(in_pixels)):
+			row = i // img_col
+			col = i % img_col
+			nodes[row][col] = in_pixels[i]
+	print('[%7.3f] %d nodes are made' % (time.time() - time_start, img_row*img_col))
+
+	# creating edge list
+	dx = [0,1,-1,0]
+	dy = [1,0,0,-1]
+	for i in range(img_row):
+			for j in range(img_col):
+					for k in range(4):
+						for cx in range(1,radius+1):
+							for cy in range(1,radius+1):
+									ni = i+cx*dx[k]
+									nj = j+cy*dy[k]
+									if (valid(ni, nj)):
+											edges.append((jarak(nodes[i][j],nodes[ni][nj]), (ni,nj), (i,j)))
+	print('[%7.3f] %d edges made' %(time.time() - time_start, len(edges)))
+
+	# sorting edge based on distance
+	edges.sort(key=lambda tup: tup[0])
+	print('[%7.3f] edges are sorted' % (time.time() - time_start))
+
+	# disjoint set initialization
+	parent = [[(i,j) for j in range(img_col)] for i in range(img_row)]
+
+	# run mst until treshold
+	edges_taken = 0
+	i = 0
+	while(edges_taken < img_row*img_col-1 and edges[i][0] <= treshold):
+			ce = edges[i]
+			pce1 = look_parent(ce[1][0], ce[1][1])
+			pce2 = look_parent(ce[2][0], ce[2][1])
+			if (pce1 != pce2):
+							parent[pce1[0]][pce1[1]] = pce2
+							edges_taken+=1
+			i+=1
+	print('[%7.3f] mst is made with %d edges' % (time.time() - time_start, edges_taken))
+
+	# add all set to list
+	parent_list = []
+	for i in range(img_row):
+		for j in range(img_col):
+			if (parent[i][j] == (i,j)):
+				parent_list.append((i,j))
+
+	# assign every pixel to its set
+	parent_i = [[0 for i in range(img_col)] for j in range(img_row)]
+	for i in range(img_row):
+		for j in range(img_col):
+			parent_i[i][j] = search(parent_list, look_parent(i, j))
+	print('[%7.3f] %d parent listed' %(time.time() - time_start, len(parent_list)))
+	
+	# count average every set
+	parsum = [(0,0,0) for i in range(len(parent_list))]
+	parcnt = [0 for i in range(len(parent_list))]
+	for i in range(img_row):
+		for j in range(img_col):
+			pari = parent_i[i][j]
+			parcnt[pari] += 1
+			parsum[pari] = (parsum[pari][0] + nodes[i][j][0],parsum[pari][1] + nodes[i][j][1],parsum[pari][2] + nodes[i][j][2])
+	print('[%7.3f] average counted' % (time.time()-time_start))
+
+	# replace all pixel with its set's average
+	for i in range(img_row):
+		for j in range(img_col):
+			pari = parent_i[i][j]
+			nodes[i][j] = (
+				parsum[pari][0]//parcnt[pari],
+				parsum[pari][1]//parcnt[pari],
+				parsum[pari][2]//parcnt[pari]
+			)
+	print('[%7.3f] replaced'%(time.time()-time_start))
+
+	# creating output
+	out_pixels = []
+	for i in range(len(in_pixels)):
+			row = i // img_col
+			col = i % img_col
+			out_pixels.append(nodes[row][col])
+	print('[%7.3f] out pixels are made' % (time.time()-time_start))
+
+	img.show()
+
+	out_img = Image.new(img.mode, img.size)
+	out_img.putdata(out_pixels)
+	out_img.save(fileout)
+	out_img.show()
+
+if __name__ == '__main__':
+	time_start = time.time()
+	if (len(sys.argv) <= 3):
+		main(sys.argv[1], sys.argv[2])
+	else:
+		main(sys.argv[1], sys.argv[2], treshold=int(sys.argv[3]))
